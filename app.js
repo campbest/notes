6c94cb0424ba8c71d496c4cad1c8fac9b797c67f
scott campbell <campbest@miamioh.edu>
2014-02-28 15:19:01 +0000

    fixed app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var notes = require('./routes/notes');
 var app = module.exports = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
@@ -40,8 +40,8 @@ app.post('/notedodestroy',notes.dodestroy);
 
 
 
-//http.createServer(app).listen(app.get('port'), function(){
- // console.log('Express server listening on port ' + app.get('port'));
-//});
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
 
